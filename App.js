75d443d60c08e55917f9e090f0e85460a45b0bb5
Venkat <venkat@codinggeek.com>
2020-06-02 10:56:21 +0530

    adding custom fonts

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+};
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (!fontsLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontsLoaded(true)}
+      />
+    );
+  }
+
   return (
     <View style={styles.rootContainer}>
-      <Text>Hello World</Text>
+      <Text style={styles.text}>Hello World</Text>
     </View>
   );
 }
@@ -13,5 +33,8 @@ const styles = StyleSheet.create({
   rootContainer: {
     paddingVertical: 50,
     paddingHorizontal: 15,
+  },
+  text: {
+    fontFamily: 'open-sans-bold',
   },
 });
